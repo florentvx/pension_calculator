675222b7b1bac3a5dbb54352aa323092577e6382
florentvx <florent.vassaux@gmail.com>
2023-10-30 23:53:00 +0000

    possibility to have multiple month in increase month

--- src/pension_simulator/pension_simulator.py
+++ src/pension_simulator/pension_simulator.py
@@ -1,6 +1,21 @@
 from __future__ import annotations
 import datetime as dt
 
+MONTH_MAP = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3, 
+    'Apr': 4, 
+    'May': 5, 
+    'Jun': 6, 
+    'Jul': 7, 
+    'Aug': 8, 
+    'Sep': 9, 
+    'Oct': 10, 
+    'Nov': 11,
+    'Dec': 12,
+}
+
 class model_statics:
     def __init__(
         self,
@@ -8,14 +23,19 @@ class model_statics:
         forward_market_rate: float,
         working_number_years: int,
         retirement_number_years: int,
-        contribution_increase_month: int,
+        contribution_increase_month_list: list[int]|list[str],
         annual_contribution_increase_rate: float,
         ):
         self.forward_inflation_rate = forward_inflation_rate
         self.forward_market_rate = forward_market_rate
         self.working_number_years = working_number_years
         self.retirement_number_years = retirement_number_years
-        self.contribution_increase_month = contribution_increase_month
+        self.contribution_increase_month_set = set([
+            mth if isinstance(mth, int) else (
+                MONTH_MAP[mth] if isinstance(mth, str) else None
+            )
+            for mth in contribution_increase_month_list
+        ])
         self.annual_contribution_increase_rate = annual_contribution_increase_rate
 
 def add_month(date: dt.date, nb_month: int = 1, set_day_to_one: bool = True):
@@ -62,8 +82,12 @@ def simulate_pension_fund(
     res = [_simulate_pension_struct(month_id, add_month(start_date, month_id) , 1.0, 0, current_amount)]
     while res[-1].month_id < statics.working_number_years * 12:
         new_contrib = res[-1].contribution if res[-1].contribution != 0 else current_contribution
-        new_contrib *= 1 + (statics.annual_contribution_increase_rate \
-            if add_month(res[-1].date).month == statics.contribution_increase_month else 0)
+        new_contrib *= 1 + (
+            statics.annual_contribution_increase_rate / len(statics.contribution_increase_month_set)
+            if add_month(res[-1].date).month in \
+                statics.contribution_increase_month_set
+            else 0
+        )
         res += [res[-1].get_next(statics.forward_inflation_rate, new_contrib, statics.forward_market_rate)]
     return res
 
